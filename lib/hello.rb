73b1385fe7a96884aef82d435921d361f13c4e5c
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 00:42:16 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,20 +1,3 @@
 def hello_t(array)
-  if block_given?
-    t_array = []
-    index = 0
-      while index < array.length
-        if array[index].start_with?("T") || array[index].start_with?("t")
-        t_array << array[index]
-        yield(array[index])
-        end
-      index += 1
-      end
-    t_array
-  else
-    puts "Hey! No block was given!"
-  end
-end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-    puts "Hi, #{name}"
-end
+end   
