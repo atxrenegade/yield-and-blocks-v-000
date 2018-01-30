96b8e6e24d33ef4e1890f5099ed38d76106e3b16
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 01:03:48 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(array)
   i = 0
   while i < array.length
-    if array[i].start_with?("T")
-      yield array[i]
-    end
+    if array[i].start_with?("T") || array[i].start_with?("t")
+      yield array[i] 
+    end  
     i += 1
   end
   array
