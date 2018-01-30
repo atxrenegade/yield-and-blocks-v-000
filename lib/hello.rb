abeecb18b30ccc356736478822098d38c4ebd04a
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 00:46:47 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,8 @@
 def hello_t(names)
   names.each do |name|
     if name.begins_with("T")
-      yield(name) {puts "Hi, #{name}"}
+      yield(name) do 
+        puts "Hi, #{name}"
     end
   end
 end
